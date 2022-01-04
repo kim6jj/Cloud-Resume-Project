194df92d12816d449f3afea16d2b0288dec50f87
JJ Kim <joungkim88@gmail.com>
2022-01-03 22:32:54 -0800

    fourth

---
+++ main.js
@@ -0,0 +1,45 @@
+// AXIOS GLOBALS
+// GET REQUEST
+function getTodos() {
+  axios
+  .get('https://6n9mqiawsc.execute-api.us-east-1.amazonaws.com/stage-2/record', {
+    timeout: 5000
+  })
+  .then(res => {
+    console.log(res)
+    showOutput(res.data)
+  })
+  .catch(err => console.error(err));
+}
+
+// POST REQUEST
+function addTodo() {
+  console.log('addToDo - POST request')
+  axios
+    .post('https://6n9mqiawsc.execute-api.us-east-1.amazonaws.com/stage-2/record', {
+      title: 'New Todo',
+      completed: false
+    })
+    .then(res => console.log(res))
+    .catch(err => console.error(err));
+}
+
+
+// AXIOS INSTANCE
+const axiosInstance = axios.create({
+  // Other custom settings
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+// axiosInstance.get('/comments').then(res => showOutput(res));
+
+// Show output in browser
+function showOutput(res) {
+  console.log(res.data)
+
+  var paragraph = document.getElementById("res");
+  var text = document.createTextNode(`Hello, Visitor #${res}!`);
+  paragraph.appendChild(text);
+}
+
+getTodos()
+addTodo()
