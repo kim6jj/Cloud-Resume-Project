34536d7df57b8e201ff61fc8f19493830d7df1de
JJ Kim <joungkim88@gmail.com>
2022-01-04 09:49:36 -0800

    10

--- main.js
+++ main.js
@@ -1,48 +1,49 @@
-// AXIOS GLOBALS
-axios.defaults.headers.common['X-Auth-Token'] =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
 
-// GET REQUEST
-function getTodos() {
-  axios
-  .get('https://ncsbce05rg.execute-api.us-east-1.amazonaws.com/beta', {
-    timeout: 5000
-  })
-  .then(res => {
-    console.log(res)
-    showOutput(res.data)
-  })
-  .catch(err => console.error(err));
-}
-
-// POST REQUEST
-function addTodo() {
-  console.log('addToDo - POST request')
-  axios
-    .post('https://ncsbce05rg.execute-api.us-east-1.amazonaws.com/beta', {
-      title: 'New Todo',
-      completed: false
+// Add your javascript here
+/* document.addEventListener('DOMContentLoaded', function () {
+    const zooming = new Zooming({
+        bgColor: 'rgb(0, 0, 0)',
+        bgOpacity: 0.8,
+        customSize: '100%'
     })
-    .then(res => console.log(res))
-    .catch(err => console.error(err));
+    zooming.listen('.img-zoomable')
+}) 
+// POST API REQUEST
+async function post_visitor() {
+    try {
+        //let response = await fetch('https://fubnpssn8l.execute-api.ca-central-1.amazonaws.com/Prod/visitor_count/', {
+        let response = await fetch('https://fubnpssn8l.execute-api.ca-central-1.amazonaws.com/Prod/visitor_count', {
+            method: 'POST',
+            headers: {
+                //'x-api-key': 'JslbDfdt1F8fl7wE4CRIj1Oqidmtmzqw4lZ539Sj'
+            }
+        });
+        let data = await response.json()
+        //console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+} */
+
+// GET API REQUEST
+async function get_visitors() {
+    // call post api request function
+    //await post_visitor();
+    try {
+        let response = await fetch('https://q0uwvjupg5.execute-api.us-east-1.amazonaws.com/visitor_count', {
+            method: 'GET',
+            headers: {
+                //'x-api-key': 'JslbDfdt1F8fl7wE4CRIj1Oqidmtmzqw4lZ539Sj',
+            }
+        });
+        let data = await response.json()
+        document.getElementById("visitors").innerHTML = data['count'] + " visits.";
+        console.log(data);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
-
-// AXIOS INSTANCE
-const axiosInstance = axios.create({
-  // Other custom settings
-  baseURL: 'https://jsonplaceholder.typicode.com'
-});
-// axiosInstance.get('/comments').then(res => showOutput(res));
-
-// Show output in browser
-function showOutput(res) {
-  console.log(res.data)
-
-  var paragraph = document.getElementById("res");
-  var text = document.createTextNode(`Hello, Visitor #${res}!`);
-  paragraph.appendChild(text);
-}
-
-getTodos()
-addTodo()
+get_visitors();
